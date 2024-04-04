41aea3f380eba328bdccaf33c0380e163573db14
bilalinit <ahmedbilalyt20@gmail.com>
2024-04-04 23:07:06 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the following operators",
+    type: "list",
+    name: "operators",
+    choices: ["addtion", "subtration", "multiply", "division"],
+  },
+]);
+if (answer.operators === "addtion") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operators === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operators === "multiply") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operators === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
